61429863f03079d561230ef8db78caada9febcc5
Anthony Damtsis <adamtsis@dius.com.au>
2013-05-09 10:46:57 +1000

    Defining data model in ToDo App

---
+++ ToDo/ToDo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDo
+//
+//  Created by Anthony Damtsis on 9/05/13.
+//  Copyright (c) 2013 DiUS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
